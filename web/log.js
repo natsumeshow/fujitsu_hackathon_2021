065f642e4c89bd0dc97bf534efca58c77edd05e7
s-wada-0 <shunsuke.wada.0@gmail.com>
2021-02-05 04:13:57 +0900

    add log.html, change styles(select, result)

---
+++ web/log.js
@@ -0,0 +1,14 @@
+//calendar
+var events = [
+    {'Date': new Date(2021, 1, 7), 'Title': '運動量: 100000'},
+    {'Date': new Date(2021, 2, 18), 'Title': '運動量: 999999'},
+    {'Date': new Date(2021, 1, 27), 'Title': '運動量: 120'},
+];
+  
+var settings = {};
+var element = document.getElementById('caleandar');
+caleandar(element, events, settings);
+
+function backToMenu(){
+    location.href = "./select.html"
+}
